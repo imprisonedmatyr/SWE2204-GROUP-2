ff1e175d1efa91835956ba89243018c9075a7e84
ag-win <io.gwin115@gmail.com>
2025-02-16 15:32:00 +0000

    changed assets to E-Library

--- E-Library/header_client.php
+++ E-Library/header_client.php
@@ -7,22 +7,22 @@
 			if (isset($_SESSION['is_staff']) && $_SESSION['is_staff'] != 1) {
 				echo strtoupper(substr($_SESSION['username'], 0, 1));
 			} else {
-				echo '<img src="img/fontawesome-free-6.6.0-web/svgs/regular/account-svgrepo-com.svg" style="width: 2.92rem; height: auto;">';
+				echo '<img src="E-Library/img/fontawesome-free-6.6.0-web/svgs/regular/account-svgrepo-com.svg" style="width: 2.92rem; height: auto;">';
 			}
 			?>
 		</div>
 		<div class="dropdown">
 			<?php if (isset($_SESSION['is_staff']) && $_SESSION['is_staff'] != 1): ?>
 				<a href="index.php" class="logo">Home</a>
-				<a href="search.php">Browse</a>
-				<a href="userdashboard.php">Dashboard</a>
-				<a href="Accountinfo.php">Profile</a>
-				<a href="favourites.php">My Library</a>
-				<a href="logout.php">Logout</a>
+				<a href="E-Library/search.php">Browse</a>
+				<a href="E-Library/userdashboard.php">Dashboard</a>
+				<a href="E-Library/Accountinfo.php">Profile</a>
+				<a href="E-Library/favourites.php">My Library</a>
+				<a href="E-Library/logout.php">Logout</a>
 			<?php else: ?>
 				<a href="index.php" class="logo">Home</a>
-				<a href="signIn.php">log In</a>
-				<a href="CreateAccount.php">Sign Up</a>
+				<a href="E-Library/signIn.php">log In</a>
+				<a href="E-Library/CreateAccount.php">Sign Up</a>
 			<?php endif; ?>
 		</div>
 	</div>
